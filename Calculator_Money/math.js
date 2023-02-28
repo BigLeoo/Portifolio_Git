a3a23e3ef740331dcf78bd3a9a6671075bc4c3ed
Leonardo <leonardo.v.m.ramoss@gmail.com>
2023-02-28 12:06:17 -0300

    js update

--- Calculator_Money/math.js
+++ Calculator_Money/math.js
@@ -8,3 +8,34 @@ function selecionarBotao(buttom){
     buttom.classList.add("selecionado");
 }
 
+function taxaEquivalente(taxaSelic, taxaDi){
+    var cdi = taxaSelic - 0.1;
+    var taxaAno = (cdi/100)*(taxaDi/100);
+    var taxaDiaria = (1+taxaAno)**(1/360)-1;
+    return taxaDiaria
+}
+
+function calculoIr(dataCompra, dataFinal){
+    var diasData = dataFinal - dataCompra;
+    return diasData
+}
+
+// const btn = document.getElementById("#btn_calcular")
+
+// btn.addEventListener("click", function() {
+//     var investimento = document.getElementById("#investimento");
+//     var taxaSelic = document.getElementById("#taxaSelic");
+//     var taxaDi = document.getElementById("#taxaDi");
+//     var dataCompra = document.getElementById("#dataCompra");
+//     var dataFinal = document.getElementById("#dataFinal");
+//     var btnVariavel = document.getElementsByClassName(".selecionado")
+
+//     if(btnVariavel == cdb){
+//         var diferencaDatas = dataCompra - dataFinal;
+
+
+//     }
+// })
+
+
+
